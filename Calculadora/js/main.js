9845ce5264ed1d4ed86bc59224d7da80a4dc8d7a
yeeiisi <127243820+yeeiisi@users.noreply.github.com>
2023-09-24 22:11:27 +0200

    Update main.js

--- Calculadora/js/main.js
+++ Calculadora/js/main.js
@@ -1,44 +1,59 @@
-const pantalla = document.querySelector(".pantalla");
-const Historial = document.querySelector(".Historial");
-const botones = document.querySelectorAll(".btn");
-const limpiarBtn = document.querySelector(".limpiar-btn"); // Selección del botón
+const pantalla = document.querySelector(".pantalla"); // Calculator screen 
+const Historial = document.querySelector(".Historial"); // Calculator history
+const botones = document.querySelectorAll(".btn"); // All buttons
+const limpiarBtn = document.querySelector(".limpiar-btn"); // CLear button
 let historial = [];
 var operacion; 
 
+
+// Save function
+// It saves the operation done in an local array
 const guardar = () => {
     localStorage.setItem("historial", JSON.stringify(historial))
     render();
 }
 
 const render = () => {
-    Historial.innerHTML = ""; // Limpiar el contenido anterior
+    Historial.innerHTML = ""; // Clean the previous content of the history
 
     historial = JSON.parse(localStorage.getItem("historial"))
 
-    if (historial === null) {
+    if (historial === null) { // Empty history
         historial = [];
     } else {
         historial.map(h => {
-            // Utilizar interpolación de cadenas para mostrar el historial correctamente
+            // Use string interpolation to display history correctly
             Historial.innerHTML += `<div class='alert alert-info text-center'>${h}</div>`;
         })
     }
 }
 
 
-// Llama a render() al cargar la página para mostrar el historial
+// Call render() to load the page if it shows the previous calculator history
 render();
 
+// Clear history function
+function clearHistorial() {
+    historial = [];
+    localStorage.setItem("historial", JSON.stringify(historial));
+    render();
+}
+
+// Buttons function 
+// When a button has been pressed
 botones.forEach(boton => {
     boton.addEventListener("click", () => {
         const botonApretado = boton.textContent;
 
-        if (boton.id === "c") {
+        // Clear (all) button
+        // It clears the calculator screen
+        if (boton.id === "c") { 
             pantalla.textContent = "0";
             return;
         }
 
-        if (boton.id === "borrar") {
+        // Backspace key
+        if (boton.id === "borrar") { 
             if (pantalla.textContent.length === 1 || pantalla.textContent === "Error") {
                 pantalla.textContent = "0";
             } else {
@@ -47,20 +62,22 @@ botones.forEach(boton => {
             return;
         }
 
+        // Equal button && Maths operations
         if (boton.id === "igual") {
             try {
                 operacion = pantalla.textContent;
-                pantalla.textContent = eval(pantalla.textContent);
+                pantalla.textContent = eval(pantalla.textContent); // Maths operations
             } catch {
-                pantalla.textContent = "Error";
+                pantalla.textContent = "Error"; // Math error
             }
-            historial.push(operacion + " = " + eval(pantalla.textContent));
-            guardar();
+            historial.push(operacion + " = " + eval(pantalla.textContent)); 
+            guardar(); // It saves the history in the array
             return;
         }
 
+        // Empty screen
         if (pantalla.textContent === "0" || pantalla.textContent === "Error") {
-            pantalla.textContent = botonApretado;
+            pantalla.textContent = botonApretado; 
         } else {
             pantalla.textContent += botonApretado;
         }
@@ -69,14 +86,10 @@ botones.forEach(boton => {
     })
 })
 
-// Implementación para limpiar el historial
+// Implementation to clear history
 limpiarBtn.addEventListener("click", () => {
     clearHistorial();
 });
 
-function clearHistorial() {
-    historial = [];
-    localStorage.setItem("historial", JSON.stringify(historial));
-    render();
-}
+
 
